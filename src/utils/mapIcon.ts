bdb0d3be5065548860266f59c048898b8d6b72a7
Thalles Ian <thallesyam@users.=.github.com>
2022-03-09 14:21:45 -0300

    chore: config axios and leaflet

---
+++ src/utils/mapIcon.ts
@@ -0,0 +1,9 @@
+import { icon } from 'leaflet'
+import iconUrl from '../../public/images/happy-icon.svg'
+
+export const mapIcon = icon({
+  iconUrl,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+})
