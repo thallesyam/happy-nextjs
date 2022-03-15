495bb6e24ad580e49fe964c2443e16ccd8014157
Thalles Ian <thallesyam@users.=.github.com>
2022-03-15 19:35:57 -0300

    chore: add and config framer motion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion'
+
 import { Header } from '../components/Header'
 import { Layout } from '../components/Layout'
 import { ContentHome } from '../components/ContentHome'
@@ -7,11 +9,26 @@ export default function Home() {
   return (
     <Layout title="Home">
       <Container>
-        <Wrapper>
-          <Header />
+        <motion.div
+          className="wrapper-logo-happy"
+          initial={{ scale: 0 }}
+          animate={{ scale: 2 }}
+          transition={{ duration: 1.5, repeat: 1, repeatType: 'reverse' }}
+        >
+          <img src="/images/happy-icon.svg" alt="Logo happy" />
+        </motion.div>
 
-          <ContentHome />
-        </Wrapper>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 3, duration: 0.8 }}
+        >
+          <Wrapper>
+            <Header />
+
+            <ContentHome />
+          </Wrapper>
+        </motion.div>
       </Container>
     </Layout>
   )
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -2,9 +2,19 @@ import styled from 'styled-components'
 
 export const Container = styled.section`
   width: 100%;
-  height: 100vh;
+  height: 100%;
+  min-height: 100vh;
+  position: relative;
 
   background: var(--bg-blue);
+
+  .wrapper-logo-happy {
+    position: absolute;
+    z-index: 10;
+    top: 50%;
+    right: 50%;
+    transform: translate(50%, -50%);
+  }
 `
 
 export const Wrapper = styled.section`
