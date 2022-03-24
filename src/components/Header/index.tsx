9e960d118fd142e6fd2d33a8df4aa5a69413577f
Thalles Ian <thallesyam@users.=.github.com>
2022-03-24 12:40:49 -0300

    feat: disable acces admin button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -13,20 +13,17 @@ export function Header() {
         <Location />
       </section>
 
-      <Link href="/admin/login" passHref>
-        <a>
-          <Button
-            bgColor="#12D4E0"
-            textHoverColor="#15C3D6"
-            hover="#96FEFF"
-            width="222px"
-            height="3.5rem"
-            fontSize="1.25rem"
-          >
-            Acesso Restrito
-          </Button>
-        </a>
-      </Link>
+      <Button
+        disabled
+        bgColor="#12D4E0"
+        textHoverColor="#15C3D6"
+        hover="#96FEFF"
+        width="222px"
+        height="3.5rem"
+        fontSize="1.25rem"
+      >
+        Acesso Restrito
+      </Button>
     </HeaderStyled>
   )
 }
